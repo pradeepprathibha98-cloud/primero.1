d3438defc14eeb9c215065d1a377ac3392cc1616
Pradeep P <pradeepprathibha98@gmail.com>
2026-08-31 22:34:11 +0530

    Update NumPy script and structure project directory

--- build-np.py
+++ build-np.py
@@ -9,10 +9,10 @@ rates = np.array([[0.9],
                   
 final_table = prices * rates
 
-# print("Original Prices:\n", prices)
-# print("Rates:\n", rates)
-# print("Final Table:(3 Countries x 5 Products)\n", final_table)
-# print("Final Table Shape:\n", final_table.shape)
+print("Original Prices:\n", prices)
+print("Rates:\n", rates)
+print("Final Table:(3 Countries x 5 Products)\n", final_table)
+print("Final Table Shape:\n", final_table.shape)
 
 # Stock Market Tracker
 
@@ -20,13 +20,13 @@ stocks = np.array([[150, 155, 160, 165, 170, 175, 180],
                    [200, 190, 180, 170, 160, 150, 140]])
 
 high_prices = stocks[stocks > 170]
-# print("high_prices:/n", high_prices)
+print("high_prices:/n", high_prices)
 
-# print("Avg per Company:\n", np.mean(stocks, axis = 1))
-# print("Avg per Day:\n", np.mean(stocks, axis = 0))
+print("Avg per Company:\n", np.mean(stocks, axis = 1))
+print("Avg per Day:\n", np.mean(stocks, axis = 0))
 
 crashed_stocks = stocks * 0.8
-# print("Crashed stocks:\n", crashed_stocks.astype(int))
+print("Crashed stocks:\n", crashed_stocks.astype(int))
 
 sales = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120])
 print("original sales:\n", sales)
@@ -45,3 +45,4 @@ print("Shape of Combined Grid:\n", combined.shape)
 
 top_month_idx = np.argmax(combined)
 print("Index of top monthly sales:\n", top_month_idx)
+
